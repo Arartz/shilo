ba7a1bdc65291905e5f7104460a9ec81d231d1d9
Arartz <shimoarnaud22@gmail.com>
2024-02-14 10:44:07 +0200

    succeded

---
+++ shimo/work.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="add.php" method="get">
+        Number1:<input type="text" name="num1"><br>
+        Number2: <input type="text" name="num2"><br>
+        <button type="submit">SUM</button>
+    </form>
+</body>
+</html>
